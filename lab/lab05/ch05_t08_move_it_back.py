76fb66ffef2ab9f380f4177eea2186f9b8465762
WongHoiLam-2627 <kennethwhl2006312@gmail.com>
2026-09-24 08:49:35 +0000

    [skip actions] lab/lab05/ch05_t08_move_it_back.py

--- lab/lab05/ch05_t08_move_it_back.py
+++ lab/lab05/ch05_t08_move_it_back.py
@@ -5,6 +5,6 @@ original = input('Enter a word:')
 if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = word[0]
-    new_word = word + fi
+    new_word = word + first + 
 else:
     print("empty")
